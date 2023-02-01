33e795103d9a6a86d6ff1191bfa14add4cf74831
ASUS <ASUS@LAPTOP-F4UON8SK>
2023-02-01 15:48:25 +0800

    First Commit

---
+++ src/TugasAkhir.java
@@ -0,0 +1,31 @@
+public class TugasAkhir {
+
+    public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 6; j++) {
+        if (i == 0 || i == 4 || j == 0 || j == 5) {
+          System.out.print("* ");
+        } else {
+          System.out.print("  ");
+        }
+      }
+      System.out.print(" ");
+      for (int j = 0; j < 5; j++) {
+        if (i == 0 || (i + j) == 4) {
+          System.out.print("* ");
+        } else {
+          System.out.print("  ");
+        }
+      }
+      System.out.print(" ");
+      for (int j = 0; j < 5; j++) {
+        if (i == 0 || i == 2 || j == 0 || j == 4) {
+          System.out.print("* ");
+        } else {
+          System.out.print("  ");
+        }
+      }
+        System.out.println();
+    }
+  }
+}
